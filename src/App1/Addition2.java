0e8ac5708aeff1079989035c8d7b6bd5109578b6
anilhavale <77318756+anilhavale@users.noreply.github.com>
2023-11-02 13:29:32 +0530

    Committing Addition2 to Branch X

---
+++ src/App1/Addition2.java
@@ -0,0 +1,20 @@
+package App1;
+
+
+
+public class Addition2 
+{
+
+	public static void main(String[] args) 
+	{
+		int a=20;
+		int b=100;
+		
+		int res=a+b;
+		
+		System.out.println("The Summation of Numbers is : "+ res);
+	}
+	
+
+	}
+
