cdb7222b5ec3b4e509a5a84d16456e131ad503e4
anilhavale <77318756+anilhavale@users.noreply.github.com>
2023-11-02 11:38:25 +0530

    First Commit....

---
+++ src/App1/Addition.java
@@ -0,0 +1,20 @@
+package App1;
+
+
+
+public class Addition 
+{
+
+	public static void main(String[] args) 
+	{
+		int a=20;
+		int b=50;
+		
+		int res=a+b;
+		
+		System.out.println("The Summation of Numbers is : "+ res);
+	}
+	
+
+	}
+
---
+++ src/App1/Subtraction.java
@@ -0,0 +1,20 @@
+package App1;
+
+
+public class Subtraction  extends Addition
+{
+
+	public static void main(String[] args) 
+	{
+	
+		int c,d;
+		c=50;
+		d=70;
+		
+		int res1=c-d;
+		
+		System.out.println("The Result 1 is : "+ res1);
+		
+	}
+
+}
